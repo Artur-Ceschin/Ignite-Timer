9fe8ac1a93e23f2751baaa3ce32e94aed05ab390
Artur Ceschin <artur.ceschin@gmail.com>
2023-06-12 14:34:37 -0300

    feat: contexts added to project

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,44 @@
+import { FormContainer, MinutesAmountMinute, TaskInput } from './styles'
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { CycleContext } from '../../../../context/CyclesContext'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CycleContext)
+
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        type="text"
+        id="task"
+        placeholder="Dê um nome para seu projeto"
+        list="task-suggestions"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 1" />
+        <option value="Projeto 1" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">Durante</label>
+      <MinutesAmountMinute
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
